f162baf02b237a72b23e82a222a991f03a02c327
moJiXiang <mojixiang1102@gmail.com>
2021-03-24 21:57:37 +0800

    增加百度知道爬虫 和 120ask爬虫

--- medicalDataSpider/spiders/babytree.py
+++ medicalDataSpider/spiders/babytree.py
@@ -1,4 +1,5 @@
 
+import re
 from scrapy.spiders import Spider
 from scrapy_splash import SplashRequest
 from medicalDataSpider.items import ArticleItem, KeywordItem, WendaAskItem, WendaReplayItem
@@ -16,6 +17,7 @@ class BabytreeSpider(Spider):
     # 爬虫名称
     name = "babytree_spider"
     keyword = ""
+    root_url = 'http://www.babytree.com'
 
     def __init__(self, keyword="", **kwargs):
         self.keyword = keyword
@@ -28,69 +30,142 @@ class BabytreeSpider(Spider):
 
     # 网页响应解析
     def parse(self, response):
-        next_page_url = response.xpath(
-            "//div[@class='pg']/a[@class='nxt']/@href").extract()[0]
-        if next_page_url:
+        current_page_number = int(response.xpath(
+            "//div[@class='pagejump']/span[@class='current']/text()").extract()[0])
+        total_page_text = response.xpath(
+            "//div[@class='pagejump']/span[@class='page-number']/text()").extract()[0]
+        result = re.search(r"\d+\.?\d*", total_page_text)
+        total_page = int(result.group())
+
+        if (current_page_number < total_page):
+
+            next_page_url = response.xpath(
+                "//div[@class='pagejump']/a[position()=last()-1]/@href").extract()[0]
             yield SplashRequest(response.urljoin(next_page_url), self.parse)
 
-        search_list = response.xpath("//ul[@class='searchList']/li")
+        search_list = response.xpath(
+            "//div[@class='search_item_area']/div[@class='search_item']")
 
         for item in search_list:
-            item_url = item.xpath(".//h3/a/@href").extract()[0]
-            if item_url.find("article") >= 0:
-                yield SplashRequest(response.urljoin(item_url), self.parse_article, args={'wait': 1}, meta={'origin_url': response.urljoin(item_url)})
+            item_url = item.xpath(
+                ".//div[@class='search_item_tit']/a/@href").extract()[0]
+            yield SplashRequest(item_url, self.parse_ask, args={'wait': 1}, meta={'origin_url': item_url})
 
-        keywordItem = KeywordItem()
-        keywordItem["title"] = "shiguanzhijia"
-        keywordItem["keywordList"] = response.xpath(
-            "//div[contains(@class, 'hotwords')]/a/text()").extract()
-        keywordItem["source"] = "http://www.shiguanzhijia.cn"
+    def parse_ask(self, response):
+        wendaAskItem = WendaAskItem()
+        wendaAskItem["keyword"] = self.keyword
+        wendaAskItem["title"] = response.xpath(
+            "//div[@class='qa-title']/h1/text()").extract()[0]
+        wendaAskItem["description"] = ""
+        wendaAskItem["images"] = []
+        wendaAskItem["content"] = response.xpath(
+            "string(//blockquote[@class='qa-text'])").extract()[0].strip()
+        wendaAskItem["addtime"] = response.xpath(
+            "//div[@class='qa-related']//span[@class='source']/abbr/text()").extract()[0]
+        wendaAskItem["source"] = response.meta["origin_url"]
 
-        yield keywordItem
+        if response.xpath("//div[@class='qa-related']/div[@class='qa-contributor']/ul/li[1]/a"):
+            wendaAskItem["username"] = response.xpath(
+                "//div[@class='qa-related']/div[@class='qa-contributor']/ul/li[1]/a/span/text()").extract()[0]
+        else:
+            wendaAskItem["username"] = response.xpath(
+                "//div[@class='qa-related']/div[@class='qa-contributor']/ul/li[1]/span/text()").extract()[0]
+        wendaAskItem["headPortrait"] = ""
+        wendaAskItem["askList"] = []
+        wendaAskItem["topicUrl"] = ""
 
-    def parse_article(self, response):
-        article = ArticleItem()
-        article["keyword"] = self.keyword
-        article["title"] = response.xpath(
-            "//div[contains(@class, 'white')]//div[@class='h hm']/h1/text()").extract()[0]
-        article["author"] = "shiguanzhijia"
-        article["visits"] = 0
-        article_info = response.xpath(
-            "string(//div[contains(@class, 'white')]//p[@class='xg1'])").extract()[0].split("|")
+        best_content = response.xpath("//div[@class='best-content']")
 
-        for info in article_info:
-            if (info.find("原作者") >= 0):
-                article["author"] = info.split("原作者: ")[1]
-            elif (info.find("查看") >= 0):
-                article["visits"] = info.split("查看: ")[1]
+        if best_content:
+            reply = WendaReplayItem()
+            reply["title"] = wendaAskItem["title"]
+            reply["username"] = best_content.xpath(
+                ".//div[@class='qa-contributor']//span[@itemprop='accountName']/text()").extract()[0].strip()
+            reply["images"] = []
+            reply["content"] = best_content.xpath(
+                "string(.//div[@class='answer-text'])").extract()[0].strip()
+            reply["addtime"] = best_content.xpath(
+                ".//li[@class='timestamp']/abbr/@title").extract()[0]
+            reply["source"] = wendaAskItem["source"]
+            reply["headPortrait"] = best_content.xpath(
+                ".//p[@class='user-avatar']/a/img/@src").extract()[0]
+            reply["likes"] = best_content.xpath(
+                ".//div[@class='qa-vote']/a/em/text()").extract()[0]
+            wendaAskItem["askList"].append(reply)
 
-        article["likes"] = 0
-        article["topicUrl"] = ""
-        article["commentList"] = []
+        reply_list = response.xpath("//ul[@class='qa-answer-list']")
 
-        comments = response.xpath("//div[@id='comment_ul']/dl")
-        if (len(comments) > 0):
-            for comment in comments:
-                _item = {}
-                _item["username"] = comment.xpath("./dt/a/text()").extract()[0]
-                _item["content"] = comment.xpath(
-                    "./dd/text()").extract()[0].strip()
-                article["commentList"].append(_item)
+        if reply_list:
+            replys = reply_list.xpath("./li[@class='answer-item']")
+            for reply in replys:
+                wendaReply = WendaReplayItem()
+                wendaReply["title"] = wendaAskItem["title"]
+                wendaReply["username"] = reply.xpath(
+                    ".//ul[@class='qa-meta']/li[@class='username']//span/text()").extract()[0]
+                wendaReply["images"] = []
+                wendaReply["content"] = reply.xpath(
+                    ".//div[@class='answer-text']/text()").extract()[0].strip()
+                wendaReply["addtime"] = reply.xpath(
+                    ".//ul[@class='qa-meta']/li[@class='timestamp']/abbr/@title").extract()[0]
+                wendaReply["source"] = wendaAskItem["source"]
+                wendaReply["headPortrait"] = reply.xpath(
+                    "./ul[@class='qa-meta']/li[@class='useravatar']/a/img/@src").extract()[0]
+                wendaReply["likes"] = reply.xpath(
+                    "./a[@class='qa-answer-list-vote']/span[@class='n']/em/text()").extract()[0]
 
-        text = []
-        div_tags = response.xpath(
-            "//td[@id='article_content']//div | //td[@id='article_content']//p")
-        for div in div_tags:
-            text.append(div.xpath("string()").extract()[0].strip())
+                wendaAskItem["askList"].append(wendaReply)
 
-        article["content"] = "<br>".join(text)
-        article["source"] = response.meta["origin_url"]
-        article["images"] = []
+        pagejump = response.xpath("//div[@class='pagejump']")
+        print("pagejump: ", pagejump)
+        if len(pagejump) > 0:
 
-        imgs = response.xpath("//td[@class='article_content']//img")
+            pages = response.xpath("//div[@class='pagejump']/a")
 
-        for img in imgs:
-            img_url = img.xpath("./@src").extract()[0]
-            article["images"].append(img_url)
+            for page in pages:
+                if page.xpath("./text()").extract()[0] == "下一页":
+                    next_page_url = page.xpath("./@href").extract()[0]
+                    print("next_page_url: ", response.urljoin(next_page_url))
+                    yield SplashRequest(response.urljoin(next_page_url), self.parse_next_ask, args={'wait': 1}, meta={'origin_url': response.urljoin(next_page_url), 'wendaAskItem': wendaAskItem})
+        else:
+            yield wendaAskItem
 
-        yield article
+    def parse_next_ask(self, response):
+        print("parse next ask")
+        wendaAskItem = response.meta["wendaAskItem"]
+        reply_list = response.xpath("//ul[@class='qa-answer-list']")
+        print("reply_list: ", reply_list)
+        if reply_list:
+            replys = reply_list.xpath("./li[@class='answer-item']")
+            for reply in replys:
+                wendaReply = WendaReplayItem()
+                wendaReply["title"] = wendaAskItem["title"]
+                wendaReply["username"] = reply.xpath(
+                    ".//ul[@class='qa-meta']/li[@class='username']//span/text()").extract()[0]
+                wendaReply["images"] = []
+                wendaReply["content"] = reply.xpath(
+                    ".//div[@class='answer-text']/text()").extract()[0].strip()
+                wendaReply["addtime"] = reply.xpath(
+                    ".//ul[@class='qa-meta']/li[@class='timestamp']/abbr/@title").extract()[0]
+                wendaReply["source"] = wendaAskItem["source"]
+                wendaReply["headPortrait"] = reply.xpath(
+                    "./ul[@class='qa-meta']/li[@class='useravatar']/a/img/@src").extract()[0]
+                wendaReply["likes"] = reply.xpath(
+                    "./a[@class='qa-answer-list-vote']/span[@class='n']/em/text()").extract()[0]
+
+                wendaAskItem["askList"].append(wendaReply)
+
+        current_page = int(response.xpath(
+            "//div[@class='pagejump']/span[@class='current']/text()").extract()[0])
+
+        total_page_text = response.xpath(
+            "//div[@class='pagejump']/span[@class='page-number']/text()").extract()[0]
+
+        result = re.search(r"\d+\.?\d*", total_page_text)
+
+        print("result: ", result.group())
+
+        total_num = int(result.group())
+
+        print("current_page: ", current_page, total_num)
+        if current_page == total_num:
+            yield wendaAskItem
